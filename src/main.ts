107f7e33228752be93c5007fcb22eb90354fab62
Sabri Gharbi <sabrigharbi99@gmail.com>
2022-02-18 13:25:51 +0100

    ready to use

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix(process.env.API_PREFIX);
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
 bootstrap();
